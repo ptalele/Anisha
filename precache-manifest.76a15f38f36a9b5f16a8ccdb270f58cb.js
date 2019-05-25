f4b7f4fcf862e10254724bdee74522d78b491981
Syamala Myneni <syamam1009@gmail.com>
2019-05-25 13:17:44 -0500

    Updates

---
+++ precache-manifest.76a15f38f36a9b5f16a8ccdb270f58cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d6e3cda5ab7848a9a9c76249c4b2a02",
+    "url": "/Anisha/index.html"
+  },
+  {
+    "revision": "20894740c308df661fa9",
+    "url": "/Anisha/static/css/main.bee35c41.chunk.css"
+  },
+  {
+    "revision": "a13e116ce43f8254531e",
+    "url": "/Anisha/static/js/2.15f70701.chunk.js"
+  },
+  {
+    "revision": "20894740c308df661fa9",
+    "url": "/Anisha/static/js/main.7b005995.chunk.js"
+  },
+  {
+    "revision": "63bfe1ccc47e911e85df",
+    "url": "/Anisha/static/js/runtime~main.28ce8f8f.js"
+  },
+  {
+    "revision": "2e3e5fef6adcbd810cf0acc7e1b6b2e7",
+    "url": "/Anisha/static/media/bkg.2e3e5fef.jpg"
+  }
+]);
